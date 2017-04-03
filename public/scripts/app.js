3ac65b1f31d9fd28f89898414afafcda7bfc3279
Kevin Schmidt <NSPtuning@gmail.com>
2017-04-03 16:52:46 -0700

    drop down menus working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,17 +5,18 @@ const Currency = {};
 Currency.names = [];
 Currency.rates = [];
 
+function populateOne(){
+  for(let prop in Currency.names.currencies){
+    let menuNames = Currency.names.currencies;
+    $('#currencyOne').append(`<option value="${prop}">${menuNames[prop]}</option>`);
+    $('#currencyTwo').append(`<option value="${prop}">${menuNames[prop]}</option>`);
+  }
+};
+
 Currency.requestNames = function(callback) {
   $.get('http://apilayer.net/api/list?access_key=0e0b2c550a586eca8af82847a443b3ed')
     .then(data => Currency.names = data, err => console.error(err))
     .then(callback);
   };
 
-Currency.requestRates = function(callback) {
-  $.get('http://apilayer.net/api/live?access_key=0e0b2c550a586eca8af82847a443b3ed')
-    .then(data => Currency.rates = data, err => console.error(err))
-    .then(callback);
-};
-
-Currency.requestRates();
-Currency.requestNames();
+Currency.requestNames(populateOne);
--- server.js
+++ server.js
@@ -9,15 +9,6 @@ const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// const conString = 'postgres://kevin:test@localhost:5432/kilovolt'
-// const client = new pg.Client(conString);
-
-// client.connect();
-// client.on('error', function(error) {
-//   console.error(error);
-// });
-
 app.use(express.static('./public'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
-
