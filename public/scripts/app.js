0132d1e8c1bb74170f4a98b8d293b74a04ceb470
Benjamin <ayzenbergb@gmail.com>
2017-04-03 15:28:35 -0700

    removed token

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,13 +6,13 @@ Currency.names = [];
 Currency.rates = [];
 
 Currency.requestNames = function(callback) {
-  $.get('http://apilayer.net/api/list?access_key=0e0b2c550a586eca8af82847a443b3ed')
+  $.get(`http://apilayer.net/api/list?access_key=${process.env.APP_TOKEN}`)
     .then(data => Currency.names = data, err => console.error(err))
     .then(callback);
   };
 
 Currency.requestRates = function(callback) {
-  $.get('http://apilayer.net/api/live?access_key=0e0b2c550a586eca8af82847a443b3ed')
+  $.get(`http://apilayer.net/api/live?access_key=${process.env.APP_TOKEN}`)
     .then(data => Currency.rates = data, err => console.error(err))
     .then(callback);
 };
