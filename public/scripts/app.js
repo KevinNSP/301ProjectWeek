e3976be4512c3c60c3cf03f4795120310f402d0b
Isak Swearingen <isakswearingen@gmail.com>
2017-04-03 15:03:50 -0700

    workingonpopulatingmenus

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,12 +11,11 @@ Currency.requestNames = function(callback) {
     .then(callback);
   };
 
-  Currency.requestRates = function(callback) {
-    $.get('http://apilayer.net/api/live?access_key=0e0b2c550a586eca8af82847a443b3ed')
-      .then(data => Currency.rates = data, err => console.error(err))
-      .then(callback);
-    };
+Currency.requestRates = function(callback) {
+  $.get('http://apilayer.net/api/live?access_key=0e0b2c550a586eca8af82847a443b3ed')
+    .then(data => Currency.rates = data, err => console.error(err))
+    .then(callback);
+};
 
-// $('#results').append(Currency.all)
 Currency.requestRates();
 Currency.requestNames();
---
+++ public/scripts/appView.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const currencies = {};
+
+// currencies.handleBaseFilter = function() {
+//   $('#currencyOne').on('change', function(){
+//     if ($(this).val()){
+//       $(Currencies.names.currencies)
+//     }
+//   })
+// }
+
+function populateOne(){
+  // $('#currencyOne').each(function(){
+  //   append()
+  // })
+};
+//
+// console.log('these are the names ', Currency.names);
+// populateOne();
