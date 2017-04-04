d0240ce43d264e121609cfc843a13cfe46648ab7
Isak Swearingen <isakswearingen@gmail.com>
2017-04-04 10:33:53 -0700

    pulled rates api, created new arrays

---
+++ public/scripts/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Currency = {};
+const Rates = {};
+let rateyRates = [];
+let moneyNames = [];
+let threeCodes = [];
+Currency.names = [];
+Rates.names = [];
+
+function populateNames(){
+  for(let prop in Currency.names.currencies){
+    let menuNames = Currency.names.currencies;
+    $('#currencyOne').append(`<option value="${prop}">${menuNames[prop]}</option>`);
+    $('#currencyTwo').append(`<option value="${prop}">${menuNames[prop]}</option>`);
+    threeCodes.push(prop);
+    moneyNames.push(menuNames[prop]);
+  }
+};
+
+function populateRates(){
+  for(let prop in Rates.names.quotes){
+    let menuNames = Rates.names.quotes;
+    $('#currencyOne').append(`<option value="${prop}">${menuNames[prop]}</option>`);
+    $('#currencyTwo').append(`<option value="${prop}">${menuNames[prop]}</option>`);
+    rateyRates.push(menuNames[prop]);
+  }
+};
+
+Rates.requestRates = function(callback) {
+  $.get('http://apilayer.net/api/live?access_key=0e0b2c550a586eca8af82847a443b3ed')
+    .then(data => Rates.names = data, err => console.error(err))
+    .then(callback);
+};
+
+Currency.requestNames = function(callback) {
+  $.get('http://apilayer.net/api/list?access_key=0e0b2c550a586eca8af82847a443b3ed')
+    .then(data => Currency.names = data, err => console.error(err))
+    .then(callback);
+};
+
+Rates.requestRates(populateRates);
+Currency.requestNames(populateNames);
---
+++ public/scripts/appView.js
@@ -0,0 +1,6 @@
+'use strict';
+
+$('Currency.names.currencies').on('click', function(){
+  return this;
+  console.log(this);
+});
