d6a494af7c0eb21bd147da733a01b2c2c94bb163
Benjamin <ayzenbergb@gmail.com>
2017-04-06 09:32:39 -0700

    negativity

--- public/scripts/model/app.js
+++ public/scripts/model/app.js
@@ -80,7 +80,7 @@ function populateRates(){
 };
 
 Rates.requestRates = function(callback) {
-  $.get('http://apilayer.net/api/live?access_key=0e0b2c550a586eca8af82847a443b3ed')
+  $.get('/apilayer/live')
     .then(data => Rates.names = data, err => console.error(err))
     .then(callback)
     .then(() =>{
@@ -97,7 +97,7 @@ Rates.requestRates = function(callback) {
 };
 
 Currency.requestNames = function(callback) {
-  $.get('http://apilayer.net/api/list?access_key=0e0b2c550a586eca8af82847a443b3ed')
+  $.get('/apilayer/list')
     .then(data => Currency.names = data, err => console.error(err))
     .then(callback)
     .then(() => {
--- server.js
+++ server.js
@@ -5,70 +5,29 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy');
-const request = require('superagent');
 const app = express();
-const currencyURL = 'http://apilayer.net/api/live?access_key=0e0b2c550a586eca8af82847a443b3ed';
-const conString = 'postgres://postgres:1234@localhost:5432/currency';
 
 const PORT = process.env.PORT || 4000;
-
-const client = new pg.Client(conString);
-client.connect();
-client.on('error', function(error) {
-  console.error(error);
-});
 
 app.use(express.static('./public'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
-app.get('/', (request, response) => response.sendFile('index.html', {root: '.'}));
+app.get('/*', (request, response) => response.sendFile('index.html', {root: '.'}));
 
-// DATABASE
-
-let currencyData = [];
-
-loadDB();
-
-function loadCurrency(){
-  client.query('SELECT COUNT(*) FROM currency')
-  .then(result => {
-    if(!parseInt(result.rows[0].count)){
-      request.get(currencyURL)
-  .then(res => {
-    currencyData = res.body;
-    console.log(currencyData);
-    currencyData.map(ele => {
-      client.query(`
-              INSERT INTO
-              currency(success, terms, privacy, currencies)
-              VALUES ($1, $2, $3, $4, $5);
-              `,
-              [ele.success, ele.terms, ele.privacy, ele.currencies]
-            ).catch(console.error);
-          })
-        }
-      )}
-  }).catch(err => console.error(err));
-};
-
-function loadDB(){
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    currency (
-        id SERIAL PRIMARY KEY,
-        success TEXT,
-        terms TEXT,
-        privacy TEXT,
-        currencies TEXT
-    )`
-  ).then(loadCurrency).catch(console.error);
+function proxyLive(request, response) {
+  console.log('Routing API request for Live Rates');
+  (requestProxy({
+    url: `http://apilayer.net/api/live?access_key=${process.env.APP_TOKEN}`
+  }))(request, response);
 }
 
-app.get('/currency', (request, response) => {
-  client.query(`
-    SELECT * FROM currency;
-    `)
-    .then(result => response.send(result.rows))
-    .catch(console.error);
-});
+function proxyList(request, response) {
+  console.log('Routing API request for List of Currencies');
+  (requestProxy({
+    url: `http://apilayer.net/api/list?access_key=${process.env.APP_TOKEN}`
+  }))(request, response);
+}
+
+app.get('/apilayer/live', proxyLive);
+app.get('/apilayer/list', proxyList);
